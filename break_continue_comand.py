73fb0566fba72b22414bdda239344f927f3d1ef6
nika2107 <borovikovanika420@gmail.com>
2024-10-11 19:02:03 +0000

    cycle_comand

---
+++ break_continue_comand.py
@@ -0,0 +1,9 @@
+for symbol in 'hello world':
+    if symbol == 'o':
+        break
+    print(i)
+ 
+for symbol in 'hello world':
+	if symbol == 'o':
+    	continue
+    print(i)
---
+++ if_else_elif.py
@@ -0,0 +1,26 @@
+if 1:
+    print('hello 1')
+ 
+a = 3
+if a > 1:
+    print(f'hello {a}')
+ 
+b = 5
+if b == 5: # Операция сравнения
+    print(f'hello {b}')
+
+    a = 3
+if a > 4:
+    print('hello 4')
+else:
+    print(f'hello {a}')
+
+
+	
+a = 3
+if a > 5:
+    print('hello 5')
+elif a < 2:
+    print('hello 2')
+else:
+    print('Tupo hello')
