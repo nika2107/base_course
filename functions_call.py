30de9d0650b4abdeb4947d060510c585c83236dc
nika2107 <borovikovanika420@gmail.com>
2024-11-15 21:25:05 +0000

    lec4

---
+++ functions_call.py
@@ -0,0 +1,14 @@
+from les_4 import mult_func
+from lec_4 import print_func
+ 
+tmp = mult_func(4)
+ 
+print(tmp)
+ 
+print(mult_func(10))
+ 
+print(mult_func('Good'))
+ 
+print_func('Hello')
+ 
+print_func(mult_func('50'))
